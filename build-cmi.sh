86de183c3ad35faa6f9b200e569d2078230679f8
mark199850 <46360948+mark199850@users.noreply.github.com>
2024-10-12 01:49:49 +0200

    build-cmi.sh: Adjust to Mark's environment

--- build-cmi.sh
+++ build-cmi.sh
@@ -10,17 +10,17 @@ start_time=$(date +%s)
 #rm -rf out
 
 # Основной каталог
-MAINPATH=/home/luffitys # измените, если необходимо
+MAINPATH=/home/mark # измените, если необходимо
 
 # Каталог ядра
-KERNEL_DIR=$MAINPATH/magictime
-KERNEL_PATH=$KERNEL_DIR/kernel_xiaomi_sm8250
+KERNEL_DIR=$MAINPATH/kernel
+KERNEL_PATH=$KERNEL_DIR/android_kernel_xiaomi_sm8250-MagicTime
 
 git log $LAST..HEAD > ../log.txt
 BRANCH=$(git branch --show-current)
 
 # Каталоги компиляторов
-CLANG19_DIR=$KERNEL_DIR/clang19
+CLANG_DIR=$KERNEL_DIR/clang20
 ANDROID_PREBUILTS_GCC_ARM_DIR=$KERNEL_DIR/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9
 ANDROID_PREBUILTS_GCC_AARCH64_DIR=$KERNEL_DIR/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
 
@@ -44,19 +44,19 @@ check_and_wget() {
         mkdir $dir
         cd $dir
         wget $repo
-        tar -zxvf Clang-19.0.0git-20240625.tar.gz
-        rm -rf Clang-19.0.0git-20240625.tar.gz
-        cd ../kernel_xiaomi_sm8250
+        tar -zxvf Clang-20.0.0git-20241013.tar.gz
+        rm -rf Clang-20.0.0git-20241013.tar.gz
+        cd ../android_kernel_xiaomi_sm8250-MagicTime
     fi
 }
 
 # Клонирование инструментов компиляции, если они не существуют
-check_and_wget $CLANG19_DIR https://github.com/ZyCromerZ/Clang/releases/download/19.0.0git-20240625-release/Clang-19.0.0git-20240625.tar.gz
+check_and_wget $CLANG_DIR https://github.com/ZyCromerZ/Clang/releases/download/20.0.0git-20241013-release/Clang-20.0.0git-20241013.tar.gz
 check_and_clone $ANDROID_PREBUILTS_GCC_ARM_DIR https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9
 check_and_clone $ANDROID_PREBUILTS_GCC_AARCH64_DIR https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
 
 # Установка переменных PATH
-PATH=$CLANG19_DIR/bin:$ANDROID_PREBUILTS_GCC_AARCH64_DIR/bin:$ANDROID_PREBUILTS_GCC_ARM_DIR/bin:$PATH
+PATH=$CLANG_DIR/bin:$ANDROID_PREBUILTS_GCC_AARCH64_DIR/bin:$ANDROID_PREBUILTS_GCC_ARM_DIR/bin:$PATH
 export PATH
 export ARCH=arm64
 
